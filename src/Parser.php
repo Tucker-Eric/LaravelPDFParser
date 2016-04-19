48938bbeecdf1d19d6470b17254dce5ecee38d70
Eric Tucker <Tucker.Ericm@gmail.com>
2016-04-19 11:11:14 -0700

    lotsa cleanup

--- src/Parser.php
+++ src/Parser.php
@@ -10,29 +10,27 @@ use Storage;
 
 class Parser extends BaseParser
 {
+
+    protected $pdf;
+
+    protected $password;
+
+    protected $has_error = false;
+
     public function parseFile($filename, $password = '')
     {
+        $this->pdf = $filename;
+        $this->password = $password;
+
         try {
-            $pdf = parent::parseFile($filename);
+            $pdf = parent::parseFile($this->pdf);
         } catch (\Exception $e) {
             //If this is a secure pdf we try unsecure it and try again
             if ($e->getMessage() === 'Secured pdf file are currently not supported.') {
-                // Relative filepath for Storage Facade
-                $file = $this->storeFileLocally($filename);
-                // Fullpath for gs command
-                $fullPath = $this->getLocalFilePath($file);
-                // Fullpath of filename for gs output
-                $unsecured = str_replace(basename($fullPath), 'u-'.basename($fullPath), $fullPath);
                 // Run the command
-                // This will return false if we can't unsecure the file
-                $this->gsRewrite($fullPath, $unsecured, $password);
                 // Call this function again and hopefully return $pdf
-                $pdf = parent::parseFile($unsecured);
-                // Clean up after ourselves when we are done
-                Storage::disk('local')->deleteDirectory(dirname($file));
+                $pdf = parent::parseFile($this->gsRewrite());
             } else {
-                // Clean up after ourselves when we are done
-                Storage::disk('local')->deleteDirectory(dirname($filename));
                 throw $e;
             }
         }
@@ -41,62 +39,55 @@ class Parser extends BaseParser
     }
 
     /**
-     * Store this file locally so we can do ghostscript stuff to it
-     * @param $filename
-     * @return string
-     */
-    public function storeFileLocally($filename)
-    {
-        $file = 'tmp/'.md5(microtime()).'/'.basename($filename);
-        $stream = fopen($filename, 'r');
-        Storage::disk('local')->put($file, $stream);
-        fclose($stream);
-
-        return $file;
-    }
-
-    /**
-     * Returns fully qualified directory root of our local storage
-     *
-     * @param string $file
-     * @return string
-     */
-    public function getLocalFilePath($file = '')
-    {
-        return config('filesystems.disks.local.root').'/'.$file;
-    }
-
-    /**
      * Rewriting for a pdf if it is encrypted to remove encryption
      *
-     * @param $original
-     * @param $new
-     * @param string $password
-     * @throws EncryptedPDFNeedsPasswordException
      * @throws IncorrectPDFPasswordException
      */
-    public function gsRewrite($original, $new, $password = '')
+    public function gsRewrite()
     {
+        // Fullpath of filename for gs output
+        $unsecured = str_replace(basename($this->pdf), 'u-'.basename($this->pdf), $this->pdf);
+
         $cmd = 'gs -q -dNOPAUSE -dBATCH -dNumRenderingThreads=2 -dNOGC -sDEVICE=pdfwrite'
-            ." -sPDFPassword=$password -sOutputFile=$new -c .setpdfwrite -f $original";
+            ." -sPDFPassword={$this->password} -sOutputFile=$unsecured -c .setpdfwrite -f {$this->pdf}";
+
         $resp = shell_exec(escapeshellcmd($cmd));
 
         if (strpos($resp, 'Error: /invalidfileaccess in pdf_process_Encrypt') !== false) {
-            $message = $password === '' ? 'Password Required To Unencrypt PDF' : 'Incorrect Password '.$password;
+            unlink($unsecured);
+            $this->has_error = true;
+            $message = empty($this->password) ? 'Password Required To Unencrypt PDF' : 'Incorrect Password '.$this->password;
             throw new IncorrectPDFPasswordException($message);
         }
+        // If we have a password, save what was done, if not leave it secured
+        if ($this->password !== null && $this->password !== '') {
+            rename($unsecured, $this->pdf);
+        }
+
+        return $this->pdf;
     }
 
+    public function hasPassword($file)
+    {
+        try {
+            $this->parseFile($file);
+        } catch (IncorrectPDFPasswordException $e) {
+            return true;
+        }
 
+        return false;
+    }
+    
     public function removePassword($file, $password)
     {
-        // Create a new file
-        $newFile = $this->getLocalFilePath($this->storeFileLocally($file));
-        // Overwrite our original with password removed
-        $this->gsRewrite($newFile, $file, $password);
-        unlink($newFile);
-        rmdir(dirname($newFile));
-
-        return $file;
+        $this->pdf = $file;
+        $this->password = $password;
+        try {
+            $this->gsRewrite();
+        } catch (IncorrectPDFPasswordException $e) {
+            return false;
+        }
+        
+        return true;
     }
 }
