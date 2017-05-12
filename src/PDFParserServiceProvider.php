af7fdb3ed1a71e4951cdbfc0c7e4627e32903394
Eric Tucker <Tucker.Ericm@gmail.com>
2017-05-12 12:54:47 -0700

    defer service provider

--- src/PDFParserServiceProvider.php
+++ src/PDFParserServiceProvider.php
@@ -6,10 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class PDFParserServiceProvider extends ServiceProvider
 {
+    protected $defer = true;
+
     public function register()
     {
         $this->app->bind('pdfparser', function () {
             return new Parser;
         });
+    }
+
+    public function provides(): array
+    {
+        return ['pdfparser'];
     }
 }
